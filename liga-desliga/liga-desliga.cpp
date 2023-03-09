170db6c86b85573a3afd1d90ee9efcdc3190fb9e
Lorena Araujo Almeida <87384833+lorenaaraujoo@users.noreply.github.com>
2023-03-09 16:46:06 -0300

    Add files via upload

---
+++ liga-desliga/liga-desliga.cpp
@@ -0,0 +1,19 @@
+int Ligado = HIGH;
+
+void setup()
+{
+  pinMode(6, INPUT);
+  pinMode(7, OUTPUT);
+}
+
+void loop()
+{ 
+  if (digitalRead(6) == 1) {
+    if (Ligado == HIGH){
+    	Ligado = LOW;
+    } else if(Ligado == LOW){
+       Ligado= HIGH;
+    };
+  };
+   digitalWrite(7, Ligado);
+}
