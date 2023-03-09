e857559743fc0ee8f1f7c08f93885d3645c12af1
Lorena Araujo Almeida <87384833+lorenaaraujoo@users.noreply.github.com>
2023-03-09 16:44:06 -0300

    Add files via upload

---
+++ liga-desliga-dois-botoes/liga-desliga.cpp
@@ -0,0 +1,17 @@
+void setup()
+{
+  pinMode(6, INPUT);
+  pinMode(7, OUTPUT);
+  pinMode(3, INPUT);
+}
+
+void loop()
+{
+  if (digitalRead(6) == 1) {
+    digitalWrite(7, HIGH);
+  }
+  if (digitalRead(3) == 1) {
+    digitalWrite(7, LOW);
+  }
+  delay(10); 
+}
